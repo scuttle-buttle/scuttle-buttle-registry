46ab1c1f0c840a494ff929b688899b57b2735e2f
Daniel Sunder <danielsunder@Daniels-MacBook-Air.local>
2021-05-20 21:53:45 +0530

    Registry Service - Initial Checkin

---
+++ src/test/java/com/scuttlebuttle/scuttlebuttleregistry/ScuttlebuttleRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scuttlebuttle.scuttlebuttleregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScuttlebuttleRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
